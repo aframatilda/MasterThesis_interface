2c985269fc55a1e232b29b327f27aa7570c86575
Afra Farkhooy <aframatilda@hotmail.com>
2023-02-24 13:08:51 +0100

    API connection works

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,56 +1,61 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
+import axios from "axios";
 
-function App() {
+class App extends Component{
 
-    const [option, setOption] = useState(0);
-    const [apiResponse, setApiResponse] = useState("");
+    constructor(props){
+        super(props);
+        this.state = { apiResponse: "" };
+    }
 
-    const callAPI = () => {
+    callAPI() {
         fetch("http://localhost:9000/testAPI")
             .then(res => res.text())
-            .then(res => setApiResponse(apiResponse));
+            .then(res => this.setState({ apiResponse: res }));
     }
 
-    const componentWillMount = () => {
-        callAPI();
+    componentWillMount() {
+        this.callAPI();
     }
 
-
-    const handleSubmit = (e) => {
+    handleSubmit = (e) => {
         e.preventDefault(); // Prevents page from doing a refresh
         console.log(option);
     }
-    
-    return (
-        <div className="App">
-            <header className="App-header">
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Please enter option:
-                        <input id="number" type="number" value={option} onChange={(e) => setOption(e.target.value)} />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
-    
-                <ol>
-                    <li> Take photo </li>
-                    <li> Get file list (video and photo) </li>
-                    <li> Delete file </li>
-                    <li> Download file </li>
-                    <li> Set exposure settings </li>
-                    <li> Set capture settings </li>
-                    <li> Take photo and download </li>
-                    <li> Stitch image and download </li>
-                    <li> Get current capture status </li>
-                    <li> Start timelapse  </li>
-                    <li> Stop timelapse  </li>
-                    <li> Get battery status  </li>
-                    <li> Get storage info  </li>
-                </ol>
-            </header>
-        </div>
-    );
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <form onSubmit={handleSubmit}>
+                        <label>
+                            Please enter option:
+                            <input id="number" type="number" value={option} onChange={(e) => setOption(e.target.value)} />
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </form>
+
+                    <h1>{apiResponse}</h1>
+                    <ol>
+                        <li> Take photo </li>
+                        <li> Get file list (video and photo) </li>
+                        <li> Delete file </li>
+                        <li> Download file </li>
+                        <li> Set exposure settings </li>
+                        <li> Set capture settings </li>
+                        <li> Take photo and download </li>
+                        <li> Stitch image and download </li>
+                        <li> Get current capture status </li>
+                        <li> Start timelapse  </li>
+                        <li> Stop timelapse  </li>
+                        <li> Get battery status  </li>
+                        <li> Get storage info  </li>
+                    </ol>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
