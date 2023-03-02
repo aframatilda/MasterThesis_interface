6683254268554cc0b34f4b4f2ad60de85cdc7c8b
Victoria Stråberg <victoria.straberg@hotmail.com>
2023-03-02 09:11:44 +0100

    wip - exec()

--- backend/routes/testAPI.js
+++ backend/routes/testAPI.js
@@ -1,16 +1,69 @@
 var express = require("express");
 var router = express.Router();
+//import {execFile,exec, parse_response} from ('child_process');
+
 
 const options = [];
 
 router.post('/', (req, res) => {
-
     const option = (req.body.option);
     options.push(option);
 });
 
 router.get('/', (req, res) => {
     res.json(options);
+    //res.json({ message: 'HEJ' });
 });
+
+
+app.get("/", function (req, res) {
+    request(options, function (err, response, body) {
+        var json = JSON.parse(body);
+        console.log(json); // Logging the output within the request function
+        res.json(request.json) //then returning the response.. The request.json is empty over here
+    }); //closing the request function      
+});
+
+/*
+router.get('/', (req, res) => {
+
+    exec('C:/Users/Victoria/Documents/GitHub/MasterThesis/bin/Release/CameraSDKTest.exe', ["option"],
+        function (error, stdout, stderr) => {
+
+        var actions = parse_response(response)
+        for action in actions {
+            exec("./camera_app $action outputfile",
+                (error, stdout, stderr) => {
+                    https.post("server.com/api/send_image/cameraid", outputfile)
+                }}
+
+    if (error) {
+            throw error;
+        }
+        console.log(stdout);
+    });
+
+});
+
+
+
+ execFile('C:/Users/Victoria/Documents/GitHub/MasterThesis/bin/Release/CameraSDKTest.exe',
+     [], (error, stdout, stderr) => {
+     if (error) {
+         throw error;
+     }
+     console.log(stdout);
+ });
+ 
+ function main() {
+     https.get("server.com/api/get_actions" response => {
+         var actions = parse_response(response)
+         for action in actions {
+             exec("./camera_app $action outputfile",
+                 (error, stdout, stderr) => {
+       https.post("server.com/api/send_image/cameraid", outputfile)
+        }}
+ */
+
 
 module.exports = router;
--- interface/src/App.js
+++ interface/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React, { useState } from "react";
+//import { exec } from "child_process";
+   //const { exec } = require('child_process');
 
 function App() {
 
     const [option, setOption] = useState([]);
-
+   
     const handleSubmit = (e) => {
         e.preventDefault();
         fetch("http://localhost:9000/testAPI", {
@@ -23,15 +25,42 @@ function App() {
             })
             .then(setOption([]))
     }
+    
 
+ /*
+    // Counts the number of directory in 
+    // current working directory
+    exec('C:/Users/Victoria/Documents/GitHub/MasterThesis/bin/Release/CameraSDKTest.exe', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: No. of directories = ${stdout}`);
+        if (stderr != "")
+            console.error(`stderr: ${stderr}`);
+    });
+*/
 
-    router.get('/' res => {
-        var actions = parse_response(res)
-        for action in actions {
-            exec("./camera_app $action outputfile", (error, stdout, stderr) =>
-            { router.post("server.com/api/send_image/cameraid", outputfile) }  }
+    /*
+  
+    execFile('C:/Users/Victoria/Documents/GitHub/MasterThesis/bin/Release/CameraSDKTest.exe',
+        [], (error, stdout, stderr) => {
+        if (error) {
+            throw error;
+        }
+        console.log(stdout);
+    });
 
-
+    
+    function main() {
+        https.get("server.com/api/get_actions" response => {
+            var actions = parse_response(response)
+            for action in actions {
+                exec("./camera_app $action outputfile",
+                    (error, stdout, stderr) => {
+          https.post("server.com/api/send_image/cameraid", outputfile)
+           }}
+    */
 
     return (
         <div className="App">
