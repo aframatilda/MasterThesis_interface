eb8ca5d3476a25cc44d01a153d4af2ee6eb70cd7
Victoria Stråberg <victoria.straberg@hotmail.com>
2023-03-08 11:27:10 +0100

    fix

--- backend/routes/testAPI.js
+++ backend/routes/testAPI.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 //import {execFile,exec, parse_response} from ('child_process');
 
-
 const options = [];
 
 router.post('/', (req, res) => {
